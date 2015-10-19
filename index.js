a6baea362cb6936daf7e3aec92093a9077f57e7d
Nathan Fritz <fritzy@netflint.net>
2015-10-19 16:34:31 -0700

    should work -- progress -- still need tests

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-module.exports = (options) => {
+'use strict';
 
+let Boom = require('boom');
+
+module.exports = (options) => {
 
   return {
     detect: (request, reply) => {
-      return (typeof request === 'object' &&
-        request.hasOwnProperty('constructor') && 
-        request.constructor.name === 'ValidationError');
+      let value = request.response;
+      return (typeof value === 'object' &&
+        value.hasOwnProperty('constructor') && 
+        value.constructor.name === 'ValidationError');
     },
-    handle (request, reply) => {
+    handle: (request, reply) => {
+      let value = request.response;
+      throw Boom.badRequest(value.error.toString());
     }
   };
+
 };
