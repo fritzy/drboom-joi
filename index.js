91efd0a3a541372d8896d78038c0bb6c2a345491
Nathan Fritz <fritzy@netflint.net>
2015-10-20 11:23:29 -0700

    tests and bug fixes

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ let Boom = require('boom');
 module.exports = (options) => {
 
   return {
-    detect: (request, reply) => {
-      let value = request.response;
+    detect: (value, request) => {
+      if (typeof value === 'object' && value.error === null && value.hasOwnProperty('value')) {
+        request.response.source = null;
+      }
       return (typeof value === 'object' &&
-        value.hasOwnProperty('constructor') && 
-        value.constructor.name === 'ValidationError');
+        value.error && 
+        value.error.name === 'ValidationError');
     },
-    handle: (request, reply) => {
-      let value = request.response;
+    handle: (value) => {
       throw Boom.badRequest(value.error.toString());
     }
   };
