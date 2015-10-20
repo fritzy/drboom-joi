5c137ff312f35e230a24f1264e03ef41dcabb859
Nathan LaFreniere <quitlahok@gmail.com>
2015-10-20 15:50:14 -0700

    rearrange it some more

--- index.js
+++ index.js
@@ -7,16 +7,12 @@ module.exports = (options) => {
   return {
     detect: (value, request) => {
       if (typeof value === 'object' && value !== null) {
-        if (value.error === null && value.hasOwnProperty('value')) {
-          request.response.source = null;
-        } else if (value.error !== null && value.error && value.error.name === 'ValidationError') {
-          return true;
-        }
+        return value.isBoom === true;
       }
       return false;
     },
     handle: (value) => {
-      throw Boom.badRequest(value.error.toString());
+      return Boom.badRequest(value);
     }
   };
 
